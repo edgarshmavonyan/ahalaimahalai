4dff7f247f572b1b5dc8b6a6c72058beb1ef87e1
edgarshmavonyan <shmavonyan.eha@phystech.edu>
2018-04-02 15:20:10 +0300

    adding 1 test

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 #include "ahalaimahalai.h"
-#include <iostream>
+#include <gtest/gtest.h>
 
-int main() {
+TEST(testIfValuesArePrecalculated, staticAssert) {
     static_assert(AhalaiMahalai<5, 5, 0>::who == 2, "The program gives an incorrect answer.");
     static_assert(AhalaiMahalai<5, 5, 0>::whence == 0, "The program gives an incorrect answer.");
     static_assert(AhalaiMahalai<5, 5, 0>::how == 0, "The program gives an incorrect answer.");
     static_assert(AhalaiMahalai<0, 0, 100>::who == 1, "The program gives an incorrect answer.");
     static_assert(AhalaiMahalai<0, 0, 100>::whence == 3, "The program gives an incorrect answer.");
     static_assert(AhalaiMahalai<0, 0, 100>::how == 100, "The program gives an incorrect answer.");
-    return 0;
+}
+
+int main(int argc, char* argv[]) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
